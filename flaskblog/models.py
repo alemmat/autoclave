2ca0832eab508fd44a582a7c4e992914c885cf45
Jorge Tammela <alemmat@gmail.com>
2021-06-08 10:30:03 -0300

    first commit

---
+++ flaskblog/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
+from flaskblog import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
+
+class Ciclo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(100), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    state = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Ciclo('{self.path}', '{self.date_posted}')"
