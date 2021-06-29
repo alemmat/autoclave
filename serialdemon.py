b4d505a8b3e7f8af744c69544536e92de8b6fc57
Jorge Tammela <alemmat@gmail.com>
2021-06-29 19:58:59 -0300

    uploadserial

--- serialdemon.py
+++ serialdemon.py
@@ -1,61 +1,117 @@
 from enum import Enum, auto
+from flaskblog.models import Ciclo
+from flaskblog import db
 import os
 import serial
+from fpdf import FPDF
+from datetime import datetime
+
+
 
 class States(Enum):
     start_cycle = auto()
     save_data_cycle = auto()
     audit = auto()
     set_time = auto()
+    write_log = auto()
 
 
 def init_serial():
     return serial.Serial('/dev/ttyAMA0')
 
 def read_serial(serial_device):
-    tdata = serial_device.read()
+    data = serial_device.read()
     time.sleep(1)
-    data_left = serial_device.inWaiting()  
-    tdata += serial_device.read(data_left)
-    print(tdata)
+    data_left = serial_device.inWaiting()
+    data += serial_device.read(data_left)
+    print(data)
+
+def read_serial_test():
+    array1=bytearray(b'\xF1\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d')
+    return array1
+
+def read_serial_test2():
+    array1=bytearray(b'\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF2')
+    return array1
 
 
+def read_serial_test3():
+    array1=bytearray(b'\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d\xF3\x4c\x6f\x72\x65\x6d\x20\x69\x70\x73\x75\x6d\x20\x64\x6f\x6c\x6f\x72\x20\x73\x69\x74\x20\x61\x6d\x65\x74\x2c\x20\x63\x6f\x6e\x73\x65\x63\x74\x65\x74\x75\x72\x0a\x0d')
+    return array1
+
+def write_file(log):
+
+    f = open("temp.txt", "a")
+    f.write(log)
+    f.close()
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size = 10)
+
+    f = open("temp.txt", "r")
+
+    for x in f:
+        pdf.cell(200, 5, txt = x, ln = 1, align = 'C')
+
+    pdf.output(datetime.utcnow().strftime('%B %d %Y - %H:%M:%S'))
+
+def create_file():
+    f = open("temp.txt", "x")
+    f.close()
 
 
-def set_time():
-    array1 = bytearray(b'\xF8')
-    array1.extend(b'\x11\x22\x33')
-    print(array1)
-    #os.system('hwclock --set %s' % date_str)
 
 
 def state_machine(state, serial_data):
 
-    if state == States.start_cycle:
+    index = 0
+    log = ""
 
-        if c == 0xF1:
-            state = States.save_data_cycle
+    while len(serial_data) > 0:
 
-        if c == 0xF4:
-            state = States.audit
+        if state == States.start_cycle:
 
-        if c == 0xF8:
-            state = States.set_time
+            if serial_data[index] == 0xF1:
+                serial_data.pop(index)
+                create_file()
+                state = States.save_data_cycle
 
-    if state == States.save_data_cycle:
+            if serial_data[index] == 0xF4:
+                state = States.audit
 
-        if c == 0xF2:
+            if serial_data[index] == 0xF8:
+                state = States.set_time
+
+        if state == States.save_data_cycle:
+
+            if serial_data[index] == 0xF2:
+                serial_data.pop(index)
+                os.remove("temp.txt")
+                state = States.start_cycle
+
+            if len(serial_data) > 0:
+                if serial_data[index] == 0xF3:
+                    serial_data.pop(index)
+                    state = States.write_log
+
+        if state == States.write_log:
+
+            log+=chr(serial_data[index])
+
+            if serial_data[index] == 0x0D:
+                print(log)
+                write_file(log)
+                state = States.save_data_cycle
+                log=""
+
+            serial_data.pop(index)
+
+        if state == States.audit:
             state = States.start_cycle
 
-        if c == 0xF3:
-            state = States.save_data_cycle
-
-    if state == States.audit:
-        state = States.start_cycle
-
-    if state == States.set_time:
-        set_time()
-        state = States.start_cycle
+        if state == States.set_time:
+            state = States.start_cycle
 
     return state
 
@@ -63,15 +119,51 @@ def state_machine(state, serial_data):
 def run_machine():
 
     state = States.start_cycle
-    serial_port = init_serial()
 
     while True:
 
-        state = state_machine(state)
+        serial_data = read_serial_test()
+        state = state_machine(state,serial_data)
+        print(state)
+        serial_data = read_serial_test3()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test2()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test3()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test2()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test3()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test2()
+        state = state_machine(state,serial_data)
+        print(state)
+
+        serial_data = read_serial_test2()
+        state = state_machine(state,serial_data)
+        print(state)
 
 
 
 if __name__ == '__main__':
-    #state_machine()
-    serial_device = init_serial()
-    read_serial(serial_device)
+    run_machine()
+    #serial_device = init_serial()
+    #read_serial(serial_device)
