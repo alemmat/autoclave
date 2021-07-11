2358b64682c79d33eb83daba6383620085e4fe11
Jorge Tammela <alemmat@gmail.com>
2021-07-11 17:01:15 -0300

    nose

--- serial_demon.py
+++ serial_demon.py
@@ -105,7 +105,7 @@ class AutoClave:
 
     def create_ciclo(self):
 
-        sqlit_insert = "INSERT INTO ciclo (path,state) VALUES (?,?,?);"
+        sqlit_insert = "INSERT INTO ciclo (path,date_created,state) VALUES (?,?,?);"
         data_tuple = ("C"+datetime.utcnow().strftime('%y_%m_%d_%H:%M')+".pdf", datetime.utcnow().strftime('%y-%m-%d %H:%M'), 0)
 
         con = sqlite3.connect('/home/pi/autoclave/flaskblog/site.db')
