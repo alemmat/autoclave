1bc9c6ba766578358cd1812506cd65e1cdbdc29c
Jorge Tammela <alemmat@gmail.com>
2021-07-12 23:41:00 +0100

    nose

--- serial_demon.py
+++ serial_demon.py
@@ -68,9 +68,7 @@ class AutoClave:
 
         for line in f:
 
-            i = i+1
-            print(29.7 * cm - 1 * cm - i * cm)
-            print(29.7 * cm - 1 * cm - i )
+            i = i + 1
             c.drawString(1 * cm, 29.7 * cm - 1 * cm - i * cm, line.replace('\r','').replace('\n',''))
 
         c.save()
