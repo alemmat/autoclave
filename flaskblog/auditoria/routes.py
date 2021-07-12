c39d22b2f7f8c48ea4e091841027e90e121f26a8
Jorge Tammela <alemmat@gmail.com>
2021-07-12 18:44:40 -0300

    nose

--- flaskblog/auditoria/routes.py
+++ flaskblog/auditoria/routes.py
@@ -6,5 +6,6 @@ auditoria = Blueprint('auditoria', __name__)
 
 @auditoria.route("/download_cycle_inform/<int:ciclo_id>")
 def download_auditoria_inform(ciclo_id):
+    path = '/home/jorge/autoclave/flaskblog/static/ciclos/'
     ciclo = Ciclo.query.get_or_404(ciclo_id)
-    return send_file('/home/jorge/autoclave/flaskblog/static/ciclos/C21_07_11_20:50.pdf')
+    return send_file(path+ciclo.path,attachment_filename='C21_07_11_20:50.pdf')
