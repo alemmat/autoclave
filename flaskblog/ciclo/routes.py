cd5114e1740a920a4cb8b36755b394bdc690c46d
Jorge Tammela <alemmat@gmail.com>
2021-07-13 12:07:35 -0300

    nose

--- flaskblog/ciclo/routes.py
+++ flaskblog/ciclo/routes.py
@@ -9,3 +9,11 @@ def download_cycle_inform(ciclo_id):
     path = '/home/pi/autoclave/flaskblog/static/ciclos/'
     ciclo = Ciclo.query.get_or_404(ciclo_id)
     return send_file(path+ciclo.name, attachment_filename=ciclo.name, as_attachment=True)
+
+
+@ciclo.route("/ciclo/<int:ciclo_id>/delete", methods=['POST'])
+def delete_ciclo(ciclo_id):
+    ciclo = Ciclo.query.get_or_404(ciclo_id)
+    db.session.delete(ciclo)
+    db.session.commit()
+    return "delete"
