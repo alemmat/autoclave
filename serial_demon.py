f5e159454afc05338d0e1b3b38d275c4a431040a
Jorge Tammela <alemmat@gmail.com>
2021-07-19 11:52:48 -0300

    nose

--- serial_demon.py
+++ serial_demon.py
@@ -64,7 +64,7 @@ class AutoClave:
         self.ciclo_id = jsonResponse["ciclo_id"]
 
     def l_insert(self):
-        response = requests.post(self.localhost+self.insert_line.format(str(self.ciclo_id)), json={'line':self.line})
+        response = requests.post(self.localhost+self.insert_cycle_line.format(str(self.ciclo_id)), json={'line':self.line})
         self.line = ""
 
     def c_cycle(self):
