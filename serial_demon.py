0d13f60a44771c4c868e2eb6b8a3d7d1c58c55ed
Jorge Tammela <alemmat@gmail.com>
2021-07-20 14:31:05 -0300

    nose

--- serial_demon.py
+++ serial_demon.py
@@ -3,7 +3,6 @@ import os
 import serial
 from datetime import datetime
 import time
-import sqlite3
 import requests
 
 
@@ -50,8 +49,8 @@ class AutoClave:
 
     def bcd_to_string(self,bcd):
 
-        string = ( ( bcd & 0xF0 ) >> 4 ) * 10
-        string = string + (bcd & 0x0F)
+        number = ( ( bcd & 0xF0 ) >> 4 ) * 10
+        number = number + (bcd & 0x0F)
 
         return string
 
@@ -59,13 +58,13 @@ class AutoClave:
 
         time = "20{year}{month}{day} {hour}:{minute}:{seconds}"
 
-        now = time.format( year = self.bcd_to_string( self.time_byte_array[0] ),
+        now = time.format(
+        year = self.bcd_to_string( self.time_byte_array[0] ),
         month = self.bcd_to_string( self.time_byte_array[1] ),
         day = self.bcd_to_string( self.time_byte_array[2] ),
         hour = self.bcd_to_string( self.time_byte_array[3] ),
         minute = self.bcd_to_string( self.time_byte_array[4] ),
         seconds = self.bcd_to_string( self.time_byte_array[5] ) )
-
 
         os.system('sudo date -u --set="%s"' % now)
 
