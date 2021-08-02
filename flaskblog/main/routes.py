db2fd195c95953789da6a8008f51d4c0400b2943
Jorge Tammela <alemmat@gmail.com>
2021-08-02 13:49:58 -0300

    nose

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, request, Blueprint, redirect, url_for
 from flaskblog.models import CompanyData
+from flask_login import login_user, current_user, logout_user, login_required
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
+@login_required
 def landing():
     companyData = CompanyData.query.first()
     return render_template('main.html', companydata = companyData)
